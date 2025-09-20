f3629dcc680ee6439b9d1dde42e17b2b22b08917
kaladoo <144223697+Kaladoo2@users.noreply.github.com>
2025-09-21 07:43:28 +0800

    Add files via upload

---
+++ hub.lua
@@ -0,0 +1,187 @@
+--[[
+disable - os.execute("stty -echo -icanon")
+enable - os.execute("stty echo icanon")
+
+bold = \27[1m
+italic = \27[3m
+
+red = \27[31m
+green = \27[32m
+yellow = \27[33m
+blue = \27[34m
+magenta = \27[35m
+cyan = \27[36m
+white = \27[37m
+reset = \27[0m
+]]--
+os.execute("stty -echo -icanon")
+os.execute("clear")
+local hostname = io.popen("hostname"):read("*a"):gsub("\n","")
+local chipset = io.popen("lspci | grep -i 'host bridge' | cut -d':' -f3- | sed 's/^ *//'"):read("*a"):gsub("\n","")
+local instructionset = io.popen("uname -m"):read("*a"):gsub("\n","")
+local cpu = io.popen("lscpu | grep 'Model name' | awk -F: '{print $2}' | sed 's/^ *//'"):read("*a"):gsub("\n","")
+local gpu = io.popen("lspci | grep -i 'vga' | cut -d':' -f3- | sed 's/^ *//'"):read("*a"):gsub("\n","")
+local osname = io.popen("lsb_release -ds | tr -d '\"'"):read("*a"):gsub("\n","")
+local ip = io.popen("hostname -I | awk '{print $1}'"):read("*a"):gsub("\n","")
+local termsize = io.popen("stty size 2>/dev/null"):read("*a"):gsub("\n","")
+local ram = io.popen("free -h | awk '/Mem:/ {print $2}'"):read("*a"):gsub("\n","")
+local storage = io.popen("df -h --output=size / | tail -n1 | sed 's/^ *//'"):read("*a"):gsub("\n","")
+local uptimes = io.popen("cat /proc/uptime | awk '{print $1}'"):read("*a"):gsub("\n","")
+local s = tonumber(uptimes)
+local d = math.floor(s/86400)
+local h = math.floor((s%86400)/3600)
+local m = math.floor((s%3600)/60)
+local uptime = d.."d "..h.."h "..m.."m"
+
+local times = 0
+
+local function aprint(txt)
+	local delay = 0.05
+	for i = 1, #txt do
+		io.write(txt:sub(i, i))
+		io.flush()
+		os.execute("sleep " .. delay)
+	end
+end
+
+while true do
+	os.execute("clear")
+	print("\27[1mLRhub\27[0m\n\27[3mv1.0.0\27[0m\n\nSelect an option\n───────────────────────────\n")
+	print("\27[33m1\27[32m System Info - Returns system info\27[0m")
+	print("\27[33m2\27[32m Testing Tool - A tool designed to mess with MAP testing\27[0m")
+	print("\27[33m3\27[32m Option - Desc\27[0m")
+	print("\27[33m4\27[32m Debugging - Opens a debugging menu\27[0m")
+	print("\n\27[33m5\27[32m Exit - Exits the program\27[0m\n")
+	
+	os.execute("stty echo icanon")
+	io.write("> ")
+	local inp = io.read()
+	inp = tonumber(inp)
+	
+	if inp and inp > 0 and inp < 6 then
+		if inp == 1 then
+			os.execute("clear")
+			print("\27[1mLRhub\27[0m\n\27[3mv1.0.0\27[0m\n\nSystem Information\n───────────────────────────\n")
+			print("\27[33mChipset\27[0m - " .. chipset)
+			print("\27[33mInstruction Set\27[0m - " .. instructionset)
+			print("\27[33mCPU\27[0m - " .. cpu)
+			print("\27[33mGPU\27[0m - " .. gpu)
+			print("\27[33mHostname\27[0m - " .. hostname)
+			print("\27[33mOS\27[0m - " .. osname)
+			print("\27[33mIP\27[0m - " .. ip)
+			print("\27[33mTerminal Size\27[0m - " .. termsize)
+			print("\27[33mRAM\27[0m - " .. ram)
+			print("\27[33mStorage\27[0m - " .. storage)
+			print("\27[33mUptime\27[0m - " .. uptime)
+			print("\n───────────────────────────\nGo back or exit? [\27[32my\27[0m/\27[31mn\27[0m]\n")
+			io.write("> ")
+			local inp = io.read()
+			if inp:lower() == "yes" or inp:lower() == "y" then
+				os.execute("clear")
+			elseif inp:lower() == "no" or inp:lower() == "n" then
+				print("Exiting...")
+				os.execute("sleep 1")
+				os.execute("clear")
+				break
+			end
+		end
+		if inp == 2 then
+			os.execute("clear")
+			print("\27[1mLRhub\27[0m\n\27[3mv1.0.0\27[0m\n\nRunning 'Testing tool'\n───────────────────────────\n")
+			
+			os.execute("cd ~/Desktop/LRhub/testingtool && python3 -u main.py")
+			break
+		end
+		if inp == 4 then
+			os.execute("clear")
+			print("\27[1mLRhub\27[0m\n\27[3mv1.0.0\27[0m\n\nDebugging Menu\n───────────────────────────\n")
+			
+			print("\27[41m  \27[42m  \27[43m  \27[44m  \n\27[45m  \27[46m  \27[47m  \27[100m  \n\27[101m  \27[102m  \27[103m  \27[104m  \n\27[105m  \27[106m  \27[107m  \27[40m  \27[0m\n")
+			local qrencode = dofile("qrencode.lua")
+			
+			local function matrix_to_string(tab, padding, padding_char, white_pixel, black_pixel)
+				local padding_string
+				local str_tab = {}
+				
+				padding_string = string.rep(padding_char, padding)
+				for i = 1, #tab + 2 * padding do
+					str_tab[i] = padding_string
+				end
+				
+				for x = 1, #tab do
+					for y = 1, #tab do
+						if tab[x][y] > 0 then
+							str_tab[y + padding] = str_tab[y + padding] .. black_pixel
+						elseif tab[x][y] < 0 then
+							str_tab[y + padding] = str_tab[y + padding] .. white_pixel
+						else
+							str_tab[y + padding] = str_tab[y + padding] .. " X"
+						end
+					end
+				end
+
+				padding_string = string.rep(padding_char, #tab)
+				for i = 1, padding do
+					str_tab[i] = str_tab[i] .. padding_string
+					str_tab[#tab + padding + i] = str_tab[#tab + padding + i] .. padding_string
+				end
+
+				padding_string = string.rep(padding_char, padding)
+				for i = 1, #tab + 2 * padding do
+					str_tab[i] = str_tab[i] .. padding_string
+				end
+
+				return str_tab
+			end
+			
+			local padding = 1
+			local padding_char
+			local black_pixel = "\27[40m  \27[0m"
+			local white_pixel = "\27[107m  \27[0m"
+			local codeword = "https://github.com/Kaladoo2/LRhub"
+
+			padding_char = padding_char or white_pixel
+
+			local ok, tab_or_message = qrencode.qrcode(codeword)
+			if not ok then
+				print(tab_or_message)
+			else
+    				local rows = matrix_to_string(tab_or_message, padding, padding_char, white_pixel, black_pixel)
+    				for i = 1, #rows do
+        				print(rows[i])
+    				end
+			end
+			
+			print("\n───────────────────────────\nGo back or exit? [\27[32my\27[0m/\27[31mn\27[0m]\n")
+			io.write("> ")
+			local inp = io.read()
+			if inp:lower() == "yes" or inp:lower() == "y" then
+				os.execute("clear")
+			elseif inp:lower() == "no" or inp:lower() == "n" then
+				print("Exiting...")
+				os.execute("sleep 1")
+				os.execute("clear")
+				break
+			end
+		end
+		if inp == 5 then
+			print("───────────────────────────\nAre you sure you want to exit? [\27[32my\27[0m/\27[31mn\27[0m]\n")
+			io.write("> ")
+			local inp = io.read()
+			if inp:lower() == "yes" or inp:lower() == "y" then
+				print("Exiting...")
+				os.execute("sleep 1")
+				os.execute("clear")
+				break
+			elseif inp:lower() == "no" or inp:lower() == "n" then
+				os.execute("clear")
+			end
+		end
+	else
+		print("\27[31mPlease choose a specified number\27[0m")
+		os.execute("stty -echo -icanon")
+		os.execute("sleep 1")
+		os.execute("stty echo icanon")
+	end
+	times = times + 1
+end
